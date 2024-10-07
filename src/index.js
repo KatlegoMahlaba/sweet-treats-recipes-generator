f505e569414a5eb96a1a34dc8c2da19804b4c9c7
Katlego Mahlaba <mahlabakatlego@gmail.com>
2024-10-07 12:51:46 +0200

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
-  let recipeElement = document.querySelector("#recipe");
-  recipeElement.innerHTML = "Recipe will go here";
-
+function displayRecipe(response) {
+  response.data.answer;
   new Typewriter("#recipe", {
-    strings: "Recipe will go here",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 200,
+    delay: 20,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionInput = document.querySelector("#user-instructions");
+  let prompt = `User instructions: Generate a sweet treats recipe about ${instructionInput.value} `;
+  let context =
+    "You love delicious treats and love to write recipes. Your mission is to write a sweet treat recipe in a basic HTML.  Make sure you follow user's instructions. ";
+  let apiKey = "0b3fb5fbb44a0fcb53400fo8t283598d";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
